d8274dc7ef9e899f58cdf8beaae2bfeb948af816
Egor Starikov <e.starikov@price-team.ru>
2021-03-20 19:32:52 +0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-for YC_VARS in $("$YC_BIN", "$YC_CONF", "$YC_CLOUD_ID", "$YC_FOLDER_ID", "$YC_OAUTH_TOKEN", "$YC_DATACENTER"); do
+for YC_VARS in $("$YC_BIN", "$YC_CONF", "$YC_CLOUD_ID", "$YC_FOLDER_ID", "$YC_TOKEN", "$YC_DATACENTER"); do
   if [ -z $1 ]; then
     echo "Cant find $1"
     exit 1
